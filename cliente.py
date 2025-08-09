47dd514f18a46e14e2966b5c26136e6c6e9353ff
gligligligli <mimigsniwi@gmail.com>
2025-08-09 20:07:02 -0300

    Criando campos de interação
    Primeiro foi feito o Label(campo de texto dentro da janela), depois os 3 botões de pedra, papel e tesoura.

--- cliente.py
+++ cliente.py
@@ -8,3 +8,16 @@ ctk.set_appearance_mode('light')
 app = ctk.CTk()
 app.title('Jokenpô')
 app.geometry('300x300')
+
+# criando os campos
+
+campo_instrucao = ctk.CTkLabel(app,text='Escolha uma das opções')
+
+#define o espaçamento do campo
+campo_instrucao.pack(pady=10)
+
+#criando botões 
+botao_pedra = ctk.CTkButton(app,text='Pedra')
+botao_papel = ctk.CTkButton(app,text='Papel')
+botao_tesoura = ctk.CTkButton(app,text='Tesoura')
+
