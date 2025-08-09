6bfc7132c3c0e57f26297eaea55e2e5b0c5686f9
gligligligli <mimigsniwi@gmail.com>
2025-08-09 19:44:57 -0300

    Criando a janela principal
    Primeiro foi definido o tema claro pra janela, em seguida, um título e por fim, o tamanho em pixels da janela, 300 de largura e 300 de altura.

--- cliente.py
+++ cliente.py
@@ -1,1 +1,10 @@
 import customtkinter as ctk
+
+# configurando a aparência
+
+ctk.set_appearance_mode('light')
+
+# criando a janela principal
+app = ctk.CTk()
+app.title('Jokenpô')
+app.geometry('300x300')
