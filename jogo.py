a9969148b968de7b337920487de2440da26acbf2
TropicTiger <patriciojr2015@gmail.com>
2025-08-11 16:20:26 -0300

    - Corrigido erro de sintaxe na função para iniciar o jogo. - A função vencedor agora printa: "O Jogador 1 Venceu!", "O Jogador 2 Venceu!" e "O jogo Empatou!"

--- jogo.py
+++ jogo.py
@@ -1,12 +1,12 @@
 class jogo:  
-    def__init__(self, id):
-    self.j1Went = False #define se o jogador 1 fez ou não sua jogada
-    self.j2Went = False #define se o jogador 2 fez ou não sua jogada
-    self.ready = False 
-    self.id = id  #cada jogo tem seu próprio id, para identificar que clientes pertencem a qual jogo
-    self.mov = [None, None] 
-    self.vitoria = [0, 0] 
-    self.empate = 0 
+    def __init__(self, id):
+        self.j1Went = False #define se o jogador 1 fez ou não sua jogada
+        self.j2Went = False #define se o jogador 2 fez ou não sua jogada
+        self.ready = False 
+        self.id = id  #cada jogo tem seu próprio id, para identificar que clientes pertencem a qual jogo
+        self.mov = [None, None] 
+        self.vitoria = [0, 0] 
+        self.empate = 0 
 
     def get_player_move(self, j): # pega o movimento do jogador , onde jogador 1 = 0 e jogador 2 = 1
         return self.mov[j]
@@ -30,17 +30,26 @@ class jogo:
 
         vencedor = -1  # R = Rocha, P = Papel, T = Tesoura 
         if j1 == "R" and j2 == "T": 
-            vencedor = 0 
+            vencedor = 0
+            print("O jogador 1 Venceu!")
         elif j1 == "T" and j2 == "R":  
             vencedor = 1 
+            print("O jogador 2 Venceu!")
         elif j1 == "P" and j2 == "R": 
             vencedor = 0 
+            print("O jogador 1 Venceu!")
         elif j1 == "R" and j2 == "P":
             vencedor = 1 
+            print("O jogador 2 Venceu!")
         elif j1 == "T" and j2 == "P": 
             vencedor = 0 
+            print("O jogador 1 Venceu!")
         elif j1 == "P" and j2 == "T": 
-            vencedor = 1 
+            vencedor = 1
+            print("O jogador 2 Venceu!")
+        elif j1 == j2: 
+            print("O jogo Empatou!")
+
 
         return vencedor  
     
