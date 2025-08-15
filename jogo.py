bd309890fc8d52e193485ad40d008682a6dbd37c
TropicTiger <patriciojr2015@gmail.com>
2025-08-14 22:37:32 -0300

    - exclusão de self.ready, visto que só era utilizado na função conexao, que também foi excluida pois a conexão passou a ser feita dentro do arquivo network. - Alteração da função jogador para receber o movimento do usuário em letras minúsculas, independente da forma como  escreva. Caso o valor recebido seja diferente de pedra, papel ou tesoura o programa interrompe a execução e retorna uma mensagem de erro. - resetWent virou só reset e passou a resetar também  o self.move.

--- jogo.py
+++ jogo.py
@@ -1,25 +1,25 @@
 class jogo:  
     def __init__(self, id):
         self.j1Went = False #define se o jogador 1 fez ou não sua jogada
-        self.j2Went = False #define se o jogador 2 fez ou não sua jogada
-        self.ready = False 
+        self.j2Went = False #define se o jogador 2 fez ou não sua jogada 
         self.id = id  #cada jogo tem seu próprio id, para identificar que clientes pertencem a qual jogo
         self.mov = [None, None] 
-        self.vitoria = [0, 0] 
-        self.empate = 0 
+       
 
-    def get_player_move(self, j): # pega o movimento do jogador , onde jogador 1 = 0 e jogador 2 = 1
-        return self.mov[j]
+    def get_player_move(self, jogador): # pega o movimento do jogador( , onde jogador 1 = 0 e jogador 2 = 1
+        return self.mov[jogador]
     
     def jogador(self, jogador, move): #atualiza os movimentos com a jogada do jogador  
-        self.mov[jogador] = move.strip().lower()
-        if jogador == 0: 
-            self.j1Went = True 
-        else: 
-            self.j2Went = True 
+       jogadas_validas = ["pedra", "papel", "tesoura"]
+       self.mov[jogador] = move.strip().lower()
+       if self.mov[jogador] not in jogadas_validas:
+           raise ValueError("Jogada inválida. Use: pedra, papel ou tesoura.")
+       if jogador == 0:
+           self.j1Went = True
+       else: 
+           self.j2Went = True
 
-    def conexao(self):  #determina se os jogadores estão ou não conectados 
-        return self.ready 
+    
     
     def bothWent(self): #determina se os dois jogadores fizeram suas jogadas 
         return self.j1Went and self.j2Went 
@@ -53,7 +53,10 @@ class jogo:
 
         return vencedor  
     
-    def resetWent(self): #reseta as jogadas 
+    def reset(self): #reseta as jogadas 
         self.j1Went = False 
         self.j2Went = False 
+        self.mov = [None, None]
+       
+
 
